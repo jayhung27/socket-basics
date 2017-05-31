bd8f206ce6074d41d77c69e382e0ffd265ff4084
Jay Hung <jayhung27@gmail.com>
2017-05-31 23:30:52 +0800

    remove Public folder

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var http = require('http').Server(app);
 
 app.use(express.static(__dirname + '/public'));
 
-http.listen(PORT, function(){
+http.listen(PORT, function () {
 	console.log('Server started!');
 });
